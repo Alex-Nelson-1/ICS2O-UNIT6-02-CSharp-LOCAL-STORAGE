2926295d5118bf2ad23bb0ff5b2ea02531950b8b
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-14 19:02:27 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -29,7 +29,7 @@ class Program
         {
             Console.WriteLine("");
         }
-        else 
+        else
         {
             Console.WriteLine("Please restart and choose y for yes, and n for no.");
         }
